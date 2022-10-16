7b1f2629413e7f21919049a8d0668e9449a4c73c
stellaw1 <stella01wang@gmail.com>
2022-10-15 18:59:52 -0700

    blog repo page wip

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -3,11 +3,13 @@ import Link from 'next/link';
 export default function BlogCard(props) {
     return (
         <Link href={`/blog/${props.slug}`}>
-            <button type="button" className="inline-flex relative items-center py-2 px-4 w-full text-sm font-medium border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
-                <svg aria-hidden="true" className="mr-2 w-4 h-4 fill-current" fill="currentColor" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
+            <button type="button" className="inline-flex relative items-center py-2 px-4 w-full text-sm font-medium border-b border-gray-400 hover:bg-gray-100 ">
+                <svg aria-hidden="true" className="ml-10 mr-3 w-5 h-5 fill-current" fill="currentColor" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
                     <path d="M 7 2 L 7 48 L 43 48 L 43 14.59375 L 42.71875 14.28125 L 30.71875 2.28125 L 30.40625 2 Z M 9 4 L 29 4 L 29 16 L 41 16 L 41 46 L 9 46 Z M 31 5.4375 L 39.5625 14 L 31 14 Z"></path>
                 </svg>
-                {props.title}
+                <div className="text-lg">
+                    {props.title}
+                </div>
             </button>
         </Link>
     )
