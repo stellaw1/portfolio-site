561e3c61ba471ce0fcd0428045f2cd271c0bfa64
stellaw1 <stella01wang@gmail.com>
2022-10-21 14:41:24 -0700

    bumps react version

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,12 +1,28 @@
-import Link from 'next/link';
 import Header from './Header';
 import Footer from './Footer';
+
+import { motion } from 'framer-motion';
+
+const variants = {
+  hidden: { opacity: 0, x: -200, y: 0 },
+  enter: { opacity: 1, x: 0, y: 0 },
+  exit: { opacity: 0, x: 0, y: -100 },
+}
 
 export default function Layout({ children }) {
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
-      <main className='container mx-auto w-1/2 flex-1 py-12 pb-24'>{children}</main>
+      <motion.main 
+        initial="hidden"
+        animate="enter"
+        exit="exit"
+        variants={variants}
+        transition={{ type: 'linear' }}
+        className='container mx-auto w-1/2 flex-1 py-12 pb-24'
+      >
+        {children}
+      </motion.main>
       <Footer />
     </div>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,18 @@ import '../styles/globals.css';
 import '../styles/fonts.css';
 import '../styles/styles.css';
 
+import { AnimatePresence } from 'framer-motion';
+
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AnimatePresence 
+    exitBeforeEnter
+    onExitComplete={() => window.scrollTo(0, 0)}
+    >
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AnimatePresence>
   );
 }
 
