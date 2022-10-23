94d398bc8179b5c973c1386a11e58b7d2e2e8668
stellaw1 <stella01wang@gmail.com>
2022-10-23 16:34:58 -0700

    remove gh actions yml

--- next.config.js
+++ next.config.js
@@ -1,18 +1,2 @@
-const isProd = process.env.NODE_ENV === 'production'
-
 module.exports = {
-  assetPrefix: isProd ? "/portfoliio-site/" : "",
-  images: {
-    unoptimized: true,
-  },
-  reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/about', // Matched parameters can be used in the destination
-        permanent: true,
-      },
-    ]
-  }
 }
