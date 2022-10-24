2915eed36337529e4b199aeb78ab5639b0778df3
stellaw1 <stella01wang@gmail.com>
2022-10-23 17:16:25 -0700

    barebones landing page

--- components/Pages/HomePage.js
+++ components/Pages/HomePage.js
@@ -3,10 +3,15 @@ import Animation from "../Layout/Animation"
 export default function HomePage() {
     return (
         <Animation>
-            <div className='prose mx-auto'>
-                <p className="w-4/5 pb-8 mx-auto place-content-center center font-bold text-center text-5xl">
-                    Stella Wang
-                </p>
+            <div className='mx-auto space-y-12'>
+                <div className="pb-12 mx-auto center font-bold text-center text-3xl sm:text-5xl relative">
+                    <div className="text-[#C0D6DF] absolute inset-1 right-px">
+                        Stella Wang
+                    </div>
+                    <div className="text-[#5FA8D3] absolute inset-0">
+                        Stella Wang
+                    </div>
+                </div>
             </div>
         </Animation>
     )
