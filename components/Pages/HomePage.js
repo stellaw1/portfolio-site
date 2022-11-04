2824dce744fd878c3dd97bb1f13046943bb7a500
stellaw1 <stella01wang@gmail.com>
2022-11-04 14:09:15 -0700

    minor hot fix

--- components/Pages/HomePage.js
+++ components/Pages/HomePage.js
@@ -16,22 +16,22 @@ export default function HomePage() {
                     </div>
                     <div className='font-bold md:inline-flex lg:flex-row md:w-auto w-full lg:items-center flex flex-col'>
                         <Link href='/project'>
-                        <a className='lg:inline-flex md:w-auto w-full mx-4 px-3 py-2 rounded items-center justify-center transition-colors duration-300 ease-in-out hover:bg-sky-500/50 text-lg lg:text-2xl hover:text-white '>
+                        <a className='lg:inline-flex md:w-auto w-full mx-4 px-3 py-2 rounded items-center justify-center transition-colors duration-300 ease-in-out hover:bg-sky-500/50 text-lg xl:text-2xl hover:text-white '>
                             Projects
                         </a>
                         </Link>
                         <Link href='/blog'>
-                        <a className='lg:inline-flex md:w-auto w-full mx-4 px-3 py-2 rounded items-center justify-center transition-colors duration-300 ease-in-out hover:bg-sky-500/50 text-lg lg:text-2xl hover:text-white'>
+                        <a className='lg:inline-flex md:w-auto w-full mx-4 px-3 py-2 rounded items-center justify-center transition-colors duration-300 ease-in-out hover:bg-sky-500/50 text-lg xl:text-2xl hover:text-white'>
                             Blog
                         </a>
                         </Link>
                         <Link href='/resume.pdf'>
-                        <a className='lg:inline-flex md:w-auto w-full mx-4 px-3 py-2 rounded items-center justify-center transition-colors duration-300 ease-in-out hover:bg-sky-500/50 text-lg lg:text-2xl hover:text-white'>
+                        <a className='lg:inline-flex md:w-auto w-full mx-4 px-3 py-2 rounded items-center justify-center transition-colors duration-300 ease-in-out hover:bg-sky-500/50 text-lg xl:text-2xl hover:text-white'>
                             Resume
                         </a>
                         </Link>
                         <Link href='/about'>
-                        <a className='lg:inline-flex md:w-auto w-full mx-4 px-3 py-2 rounded items-center justify-center transition-colors duration-300 ease-in-out hover:bg-sky-500/50 text-lg lg:text-2xl hover:text-white'>
+                        <a className='lg:inline-flex md:w-auto w-full mx-4 px-3 py-2 rounded items-center justify-center transition-colors duration-300 ease-in-out hover:bg-sky-500/50 text-lg xl:text-2xl hover:text-white'>
                             About
                         </a>
                         </Link>
